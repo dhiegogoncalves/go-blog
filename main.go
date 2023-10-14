777833158ea958c7fd136f8dbedfdb4943164803
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2023-10-13 23:14:52 -0400

    feat: add viper.

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"github.com/dhiegogoncalves/go-blog/config"
+	"github.com/spf13/viper"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	configs := configSet()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run("localhost:8080")
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the configs")
+	}
+
+	var configs config.Config
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Println("Error unmarshalling the configs")
+	}
+
+	return configs
 }
